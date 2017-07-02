7babaf740873c932e9e8cba7558305c2288b7da7
Apollo Tang <tang.apollo@gmail.com>
2017-07-02 17:25:08 -0400

    added circle button

--- 02-todo-w-ui-control/src/todo-app/containers/add-todo/index.js
+++ 02-todo-w-ui-control/src/todo-app/containers/add-todo/index.js
@@ -3,28 +3,28 @@ import {connect} from 'react-redux';
 
 import {mapStoreToProps, mapDispatchToProps} from './selector';
 
-
+import style from './style.less';
 const AddTodo = ({dispatch_addTodo}) => {
   let inputValue;
   let inputNode;
   return (
-    <div>
-      <form onSubmit={
-        e=>{
-          e.preventDefault();
-          dispatch_addTodo(inputValue);
-          inputValue = '';
-          inputNode.value = '';
-        }} >
-        <input
-          ref={ thisNode=>{ inputNode = thisNode }}
-          onChange={e=>{ e.preventDefault(); inputValue=e.target.value;}}
-        />
-        <button type="submit">Add Todo</button>
-      </form>
+    <div className={`section_addTodo ${style['module-style']}`} >
+      <input className="todoTextInput"
+        defaultValue="What needs to be done?"
+        ref={ thisNode=>{ inputNode = thisNode }}
+        onChange={e=>{ e.preventDefault(); inputValue=e.target.value;}}
+      />
+      <button className="addTodo" onClick={()=>{dispatch_addTodo(inputValue)}}>
+        <i className="fa fa-plus" aria-hidden="true"></i>
+      </button>
+      <button className="x" onClick={()=>{dispatch_addTodo(inputValue)}}><i className="fa fa-times" aria-hidden="true"></i></button>
+      <button className="done" onClick={()=>{dispatch_addTodo(inputValue)}}><i className="fa fa-check" aria-hidden="true"></i></button>
     </div>
   );
 };
 
+//      <button className="addTodo" onClick={()=>{dispatch_addTodo(inputValue)}}><i className="fa fa-check" aria-hidden="true"></i></button>
+//      <button className="x" onClick={()=>{dispatch_addTodo(inputValue)}}><i className="fa fa-times" aria-hidden="true"></i></button>
+//      <button className="plus" onClick={()=>{dispatch_addTodo(inputValue)}}><i className="fa fa-plus" aria-hidden="true"></i></button>
 
 export default connect(mapStoreToProps, mapDispatchToProps)(AddTodo);
