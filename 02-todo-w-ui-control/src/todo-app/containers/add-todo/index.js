10e907220a40263489dec0b4ecd408e84806c535
Apollo Tang <tang.apollo@gmail.com>
2017-07-02 17:39:34 -0400

    move circleButton into a style widget

--- 02-todo-w-ui-control/src/todo-app/containers/add-todo/index.js
+++ 02-todo-w-ui-control/src/todo-app/containers/add-todo/index.js
@@ -13,18 +13,12 @@ const AddTodo = ({dispatch_addTodo}) => {
         defaultValue="What needs to be done?"
         ref={ thisNode=>{ inputNode = thisNode }}
         onChange={e=>{ e.preventDefault(); inputValue=e.target.value;}}
-      />
+        />
       <button className="addTodo" onClick={()=>{dispatch_addTodo(inputValue)}}>
         <i className="fa fa-plus" aria-hidden="true"></i>
       </button>
-      <button className="x" onClick={()=>{dispatch_addTodo(inputValue)}}><i className="fa fa-times" aria-hidden="true"></i></button>
-      <button className="done" onClick={()=>{dispatch_addTodo(inputValue)}}><i className="fa fa-check" aria-hidden="true"></i></button>
     </div>
   );
 };
-
-//      <button className="addTodo" onClick={()=>{dispatch_addTodo(inputValue)}}><i className="fa fa-check" aria-hidden="true"></i></button>
-//      <button className="x" onClick={()=>{dispatch_addTodo(inputValue)}}><i className="fa fa-times" aria-hidden="true"></i></button>
-//      <button className="plus" onClick={()=>{dispatch_addTodo(inputValue)}}><i className="fa fa-plus" aria-hidden="true"></i></button>
 
 export default connect(mapStoreToProps, mapDispatchToProps)(AddTodo);
